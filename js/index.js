14ce41ddbb787c2d233f4db74d04a954413e7eaa
Gaston Pucciarelli <gaston_pucciarelli@hotmail.com>
2024-05-06 20:40:59 -0300

    Reform page

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function cangePage(id){
+    let id= document.getElementsByClassName(".cardUX");
+
+    if (id == "cardUX") {
+        location.href="ux.html"
+    } else {
+        location.href="web.html"
+    }
+}
--- js/main.js
+++
@@ -1,6 +0,0 @@
-const scrollNav = () => {
-  const divNav = document.querySelector(".navTop")
-  let windowsScroll = window.scrollY > 250
-  divNav.classList.toggle("active", windowsScroll)
-}
-window.addEventListener("scroll", scrollNav)
